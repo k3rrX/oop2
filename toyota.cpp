b2ff971bd86c61ecb418ea0cdc2e5e2c60303204
k3rrx <65369862+k3rrX@users.noreply.github.com>
2025-12-10 21:38:55 +0300

    Update toyota.cpp

--- toyota.cpp
+++ toyota.cpp
@@ -1,40 +1,28 @@
 #include <iostream>
 #include <string>
 
-class Money {
+struct Money {
     char d[13];
-public:
-    Money() { for(int i=0;i<13;i++) d[i]=0; }
-    
-    Money(std::string s) {
+    Money(std::string s="0") {
         for(int i=0;i<13;i++) d[i]=0;
-        int r=0,k=0,i=0;
-        
-        while(i<s.size() && s[i]!='.' && s[i]!=',')
-            if(s[i]>='0' && s[i]<='9')
-                r = r*10 + (s[i++]-'0');
-        
-        i++; 
-        
-        if(i<s.size()) k = (s[i]-'0')*10;
-        if(i+1<s.size()) k += s[i+1]-'0';
-        
+        int r=0,k=0,p=0;
+        while(p<s.size() && s[p]!='.' && s[p]!=',')
+            r = r*10 + s[p++]-'0';
+        p++;
+        if(p<s.size()) k = (s[p]-'0')*10;
+        if(p+1<s.size()) k += s[p+1]-'0';
         d[0]=k%10; d[1]=k/10;
-        for(int j=2;r>0 && j<13;r/=10)
-            d[j++] = r%10;
+        for(int i=2;r;r/=10) d[i++]=r%10;
     }
-    
-    void show() {
+    void print() {
         int i=12;
-        while(i>=2 && d[i]==0 && i>2) i--;
-        for(;i>=2;i--) std::cout<<(char)(d[i]+'0');
-        std::cout<<'.'<<(char)(d[1]+'0')<<(char)(d[0]+'0');
+        while(i>2 && !d[i]) i--;
+        for(;i>=0;i--)
+            std::cout << (i==1?'.':'\0') << (char)(d[i]+'0');
     }
 };
 
 int main() {
     Money m("1234.56");
-    std::cout << "Сумма: ";
-    m.show();
-    return 0;
+    m.print();
 }
